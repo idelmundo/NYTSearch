464e689baab2488c795c63ce2e3811194cf60561
jdavis3333 <jdavis3333@gmail.com>
2020-06-26 14:29:29 -0700

    query URL added

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
-    something
-
-
+    var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=lqFEf9JuRfkMY8PD9lKKgGAULbE4QPm9";
+    console.log(queryUrl);
 
 
 
